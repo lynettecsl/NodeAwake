577c86e1f39ce73bad444b8602a14d9ca6de44ec
Gabriel Tay <gabrieltaykl@gmail.com>
2018-08-24 09:38:09 +0800

    Express module

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-var http = require('http');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => debug(`Listening on port ${PORT}`));
 
 setInterval(function() {
 	http.get('https://nodestocks.herokuapp.com/');
+}, 300000); // every 5 minutes (300000)
+
+setInterval(function() {
+	http.get('https://nodeawake.herokuapp.com/');
 }, 300000); // every 5 minutes (300000)
