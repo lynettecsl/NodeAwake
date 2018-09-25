5c399c85a79824985164734c4bdfbea644d0cd1a
lynettecsl <43583515+lynettecsl@users.noreply.github.com>
2018-09-26 01:47:49 +0800

    Update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 setInterval(function() {
-	https.get('https://nodestocks.herokuapp.com/');
+	https.get('https://resourcebooking.herokuapp.com/');
 }, 300000); // every 5 minutes (300000)
 
 setInterval(function() {
